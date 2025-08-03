b3354940d80a649d7d897db5f5824e1f15fe2aae
Tair Sayranbekov <tairtopskiy@gmail.com>
2025-08-03 17:42:32 +0500

    scss

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  env: {
+    NEXT_PUBLIC_API_URL: 'http://127.0.0.1:8000',
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'www.gstatic.com' },
+      { protocol: 'https', hostname: 'ssl.gstatic.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+    prependData: `
+      @import "mixins.scss";
+      @import "vars.scss";
+      @import "extends.scss";
+    `,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
